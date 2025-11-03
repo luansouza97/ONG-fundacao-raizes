bd1b6034226bd38a25470bac27ff9bd0eee7577e
luansouza97 <souza.luansousa97@gmail.com>
2025-11-03 18:56:43 -0300

    Atualização

--- assets/js/global/menu.js
+++ assets/js/global/menu.js
@@ -1,9 +1,12 @@
 const btMenu = document.querySelector('#menu-check')
-const iconMenu = document.querySelector('.icon-menu')
+const iconMenu = document.querySelector('.header__menu-hamburguer')
 const menu = document.querySelector('.header__menu')
+const ul = document.querySelector('ul')
+
 
 
 document.addEventListener('click', (e) => {
+    
     if(!menu.contains(e.target) && !btMenu.contains(e.target)){
         menu.style.transform = 'translateX(100%)'
     }
